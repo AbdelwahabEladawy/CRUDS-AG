2cfa1d5b90ec6e66f11e8c670586af9ddc7b10b3
abdelwhab Eladawy <abdelwahabeladawy@gmail.com>
2024-06-01 11:43:29 +0300

    finish the function of create product , clear data , save to local storage

--- js/index.js
+++ js/index.js
@@ -6,10 +6,40 @@ var total = document.getElementById("total");
 var category = document.getElementById("category");
 var count = document.getElementById("count");
 var create = document.getElementById("create");
+var productList;
+if(localStorage.product !=null){
+  productList=JSON.parse(localStorage.product)
+}else{
+  productList=[]
+}
+
 
 function calcTotal() {
   var result=(+price.value+ +ads.value) - discount.value
 total.innerHTML=result
 }
 
+create.onclick=function createElement(){
+var products={
+  name:title.value,
+  price:price.value,
+  ads:ads.value,
+  discount:discount.value,
+  total:total.innerHTML,
+  category:category.value,
+  count:count.value
+}
+productList.push(products)
+localStorage.setItem("product",JSON.stringify(productList))
+console.log(productList)
+clearData()
+}
 
+function clearData(){
+  title.value=""
+  price.value=""
+  ads.value=""
+  discount.value=""
+  category.value=""
+  count.value=""
+}
